8a049d7c8b9e3e2376c068208ef94771b432b9d7
Edson Paul <edsonpaul@eeMac.local>
2022-09-04 23:34:39 +0800

    add movie_reviews_dag

--- dags/test_dag/test_dag.py
+++ dags/test_dag/test_dag.py
@@ -2,27 +2,31 @@ from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.email_operator import EmailOperator
 from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.models import Variable
 from datetime import datetime, timedelta
+import os
 
 # Variables used by tasks
-
+DAG_ID = os.path.basename(__file__).replace(".pyc", "").replace(".py", "")
 
 # Default args
 default_args ={
-    'owner': 'edsonpaul',
+    'owner': 'airflow',
     'retries': 5,
     'retry_delay': timedelta(minutes=5),
+    'email': ['info@example.com']
     'email_on_failure': False
+    'email_on_retry': False
 }
 
 # Instantiate DAG
 with DAG (
-    dag_id='test_dag_v1',
+    DAG_ID,
     default_args=default_args,
     start_date=datetime(2022, 9, 1),
     schedule_interval='0 0 * * *',
     catchup=False,
-    #template_searchpath='include' #include path to look for external files
+    #template_searchpath='{DAG_ID}/include/' #include path to look for external files
 ) as dag:
 
     task0 = DummyOperator(task_id='start')
