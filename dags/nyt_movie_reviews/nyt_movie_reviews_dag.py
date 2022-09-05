a65961141bded99b666f33bb26299674a11f15ba
Edson Paul <edsonpaul@eeMac.local>
2022-09-05 16:28:31 +0800

    added s3_to_postgres func

--- dags/nyt_movie_reviews/nyt_movie_reviews_dag.py
+++ dags/nyt_movie_reviews/nyt_movie_reviews_dag.py
@@ -2,6 +2,7 @@ from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.email_operator import EmailOperator
 from airflow.operators.python import PythonOperator
+from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.utils.task_group import TaskGroup
 from airflow.models import Variable
 
@@ -9,7 +10,7 @@ from datetime import datetime, timedelta
 import os
 
 # Local modules
-from nyt_movie_reviews.utils import api_to_s3
+from nyt_movie_reviews.utils import api_to_s3, s3_to_postgres
 
 # Variables used by tasks
 DAG_ID = os.path.basename(__file__).replace(".pyc", "").replace(".py", "")
@@ -38,7 +39,7 @@ with DAG (
     schedule_interval='0 0 * * *',
     tags=["nyt"],
     catchup=False,
-    #template_searchpath='{DAG_ID}/include/' #include path to look for external files
+    # template_searchpath='/Users/edsonpaul/Desktop/airflow_docker/dags/{}/sql '.format(DAG_ID) #include path to look for external files
 ) as dag:
 
     task0 = DummyOperator(task_id='start')
@@ -47,7 +48,7 @@ with DAG (
     with TaskGroup('api_paths_extract') as api_paths_extract:
         for path in paths:
             extracts = PythonOperator(
-                task_id="{0}_api_to_s3".format(path),
+                task_id="{}_api_to_s3".format(path),
                 dag=dag,
                 python_callable=api_to_s3,
                 op_kwargs={
@@ -57,25 +58,34 @@ with DAG (
                     "bucket_name": bucket_name,
                 }
             )
-
-    # task2 = PythonOperator(
-    #     task_id="critics_api_to_s3",
-    #     dag=dag,
-    #     python_callable=api_to_s3,
-    #     op_kwargs={
-    #         "path": "critics",
-    #         "access_key": NYT_API_TOKEN,
-    #         "secret_key": NYT_API_SECRET,
-    #         "bucket_name": bucket_name,
-    #         }
-    # )
+    with TaskGroup('create_postgres_table') as create_postgres_table:
+        for path in paths:
+            create_sql_script = PostgresOperator(
+                task_id="{}_create_table".format(path),
+                dag=dag,
+                postgres_conn_id='postgres_localhost',
+                sql='sql/create_{}_table.sql'.format(path)
+            )
+    
+    # Define Task Group with Python Operator S3 loads. Loop through paths provided
+    with TaskGroup('load_to_postgres') as load_to_postgres:
+        for path in paths:
+            extracts = PythonOperator(
+                task_id="{}_s3_to_postgres".format(path),
+                dag=dag,
+                python_callable=s3_to_postgres,
+                op_kwargs={
+                    "path": path,
+                    "bucket_name": bucket_name,
+                }
+            )
 
     # Define task to send email
-    send_email = EmailOperator(
-        task_id='send_email',
-        to=email_notification_list,
-        subject='NYT Movie Reviews DAG',
-        html_content='<p>The NYT Movie Reviews DAG completed successfully. <p>'
-    )
+    # send_email = EmailOperator(
+    #     task_id='send_email',
+    #     to=email_notification_list,
+    #     subject='NYT Movie Reviews DAG',
+    #     html_content='<p>The NYT Movie Reviews DAG completed successfully. <p>'
+    # )
 
-    task0 >> api_paths_extract >> send_email
+    task0 >> api_paths_extract >> create_postgres_table >> load_to_postgres
--- dags/nyt_movie_reviews/utils.py
+++ dags/nyt_movie_reviews/utils.py
@@ -1,13 +1,12 @@
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 from airflow.providers.postgres.hooks.postgres import PostgresHook
-from airflow.providers.postgres.operators.postgres import PostgresOperator
 
+import json
+from tempfile import NamedTemporaryFile
 import logging
 
 def api_to_s3(path, access_key, secret_key, bucket_name, ds_nodash) -> None:
     import requests
-    import json
-    from tempfile import NamedTemporaryFile as ntf
 
     headers = {
         "Accept" : "application/json",
@@ -20,7 +19,7 @@ def api_to_s3(path, access_key, secret_key, bucket_name, ds_nodash) -> None:
 
     # print(response)
 
-    with ntf(mode='w', suffix=f"{ds_nodash}") as temp_json_file:
+    with NamedTemporaryFile(mode='w', suffix=f"{ds_nodash}") as temp_json_file:
         json.dump(data, temp_json_file, ensure_ascii=False, indent=4)
         temp_json_file.flush() # save file to disk
 
@@ -34,12 +33,105 @@ def api_to_s3(path, access_key, secret_key, bucket_name, ds_nodash) -> None:
             replace=True
         )
 
-def s3_to_postgres() -> None:
+
+def s3_to_postgres(path, bucket_name, ds_nodash) -> None:
+    import pandas as pd
+    import os
+    import psycopg2
+    import psycopg2.extras as extras
+    import numpy as np
 
     # Step1: Connect to s3 bucket to get files
     # Loops through files in each folder and store them in df
+    s3_hook = S3Hook(aws_conn_id="s3_minio_conn")
+    #postgres_hook = PostgresHook(postgres_conn_id="postgres_localhost")
+
+    key=f"{path}/nyt_{path}_{ds_nodash}.json"
+    try:
+        file_name = s3_hook.download_file(key, bucket_name)
+    except Exception as error:
+        print(error)
+
+    print(f"Successfully downloaded {file_name}")
+
+    # Rename downloaded file from the arbitrary file name created by s3_hook download
+    downloaded_file_path = '/'.join(file_name.split('/')[:-1])
+    new_file_name = f"{downloaded_file_path}/{path}.json"
+    os.rename(src=file_name, dst=new_file_name)
+
+    print(f"After rename: {new_file_name}.")
+    
+    with open(new_file_name, 'r') as j:
+        contents = json.loads(j.read())
+    
+    
+    df = pd.DataFrame()
+    for result in contents['results']:
+        df = df.append(result, ignore_index=True)
+    # Use dataframe apply for json field
+    df['multimedia'] = df['multimedia'].apply(json.dumps)
+
+    # df = df.replace(r'\r+|\n+|\t+','', regex=True)
+    # df = df.replace(to_replace=[r"\\t|\\n|\\r", "\t|\n|\r"], value=["",""], regex=True, inplace=True)
+
+    try:
+        conn = psycopg2.connect(
+            host="host.docker.internal",
+            database="test",
+            user="airflow",
+            password="airflow")
+        
+        # create a cursor
+        cursor = conn.cursor()
+        
+	    # execute a statement
+        print('PostgreSQL database version:')
+        #cursor.execute('SELECT version()')
+
+        # display the PostgreSQL database server version
+        db_version = cursor.fetchone()
+        print("DB version {}".format(db_version))
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    
+    logging.info("Connection to postgres established.")
+
+    # create the table if it does not already exist
+    # cursor.execute(open(sql_query, "r").read())
+    # conn.commit()
+
+    tuples = [tuple(x) for x in df.to_numpy()]
+    cols = ','.join(list(df.columns))
+    # SQL query to execute insert
+    query = "INSERT INTO %s(%s) VALUES %%s" % (path, cols) # path is also the tables name
+
+    extras.register_default_json(loads=lambda x: x)
+    extras.execute_values(cursor, query, tuples)
+    conn.commit()
+
+    cursor.close()
+
+    if conn is not None:
+        conn.close()
+        print('Database connection closed.')
 
 
-    # Step 2: Connect to postgres using PostgresHook
-    # Read the data from df and load them using PostGresOperator
-    hook = PostgresHook(postgres_conn_id="postgres_localhost")
+
+    # DATABASE_LOCATION = 'postgresql://airflow:airflow@host.docker.internal:5432/test'
+    # DATABASE_LOCATION = 'postgresql://airflow:airflow@localhost:5432/test'
+    # engine = sqlalchemy.create_engine(DATABASE_LOCATION)
+    # print(f"Engine: {engine}")
+    # df.to_sql(
+    #     "critics",
+    #     engine,
+    #     schema='public',
+    #     index=False,
+    #     if_exists='append',
+    #     dtype={"bets": sqlalchemy.types.JSON}
+    # )
+
+    # try:
+    #     logging.info("Trying to load the data.")
+        
+    # except:
+    #     print("Data already exists in the database")
